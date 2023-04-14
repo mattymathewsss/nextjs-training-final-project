24565641e14fb581ec7abe78c154a16f542958c1
Bryan Matias <bmm.matty@gmail.com>
2023-04-14 09:51:47 +0000

    storybook fixes

--- components/ArticleCard/ArticleCard.mock.ts
+++ components/ArticleCard/ArticleCard.mock.ts
@@ -1,7 +1,11 @@
 import { type IArticleCard } from './ArticleCard';
 
 const base: IArticleCard = {
-  sampleTextProp: 'For Developers use only',
+  title: 'Black Hole Alert: Initially identified as a galaxy maybe a threat to Earth',
+  author: 'Keith McNulty',
+  authorImageUrl: 'https://miro.medium.com/v2/resize:fill:132:132/1*5jw92xZWkTltN6nnAF3D1A.png',
+  description: 'Discover stories, thinking, and expertise from writers on any topic.',
+  size: 'base'
 };
 
 export const mockArticleCardProps = {
--- components/Banner/Banner.stories.tsx
+++ components/Banner/Banner.stories.tsx
@@ -1,21 +1,43 @@
-import { type ComponentMeta, type ComponentStory } from '@storybook/react';
-import Banner, { type IBanner } from './Banner';
-import { mockBannerProps } from './Banner.mock';
+import { type ComponentMeta, type ComponentStory } from '@storybook/react'
+import Button from 'components/Button/Button'
+import Image from 'next/image'
+import Banner from './Banner'
 
 export default {
   title: 'Components/Banner',
   component: Banner,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
-} as ComponentMeta<typeof Banner>;
+} as ComponentMeta<typeof Banner>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Banner> = (args) => (
-  <Banner {...args} />
-);
+const Template: ComponentStory<typeof Banner> = (args) => <Banner {...args} />
 
-export const Base = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const Base = Template.bind({})
 Base.args = {
-  ...mockBannerProps.base,
-} as IBanner;
+  backgroundColor: 'bg-amber-400',
+  primaryContent: (
+    <>
+      <h1 className="mb-6 text-7xl">Stay Curious</h1>
+      <p className="mb-6 text-xl">
+        Discover stories, thinking, and expertise from writers on any topic.
+      </p>
+      <Button
+        className="w-40"
+        buttonType="btn-primary"
+        content="Start Reading"
+      />
+    </>
+  ),
+  secondaryContent: (
+    <div className="relative -mt-5 hidden h-96 w-96 md:inline-flex lg:h-96">
+      <Image
+        src="/assets/medium-logo.png"
+        alt="Logo"
+        fill
+        className="rounded-full"
+      />
+    </div>
+  ),
+}
