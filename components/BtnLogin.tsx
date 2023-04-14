f6ab3c58a1cc4c42a84b1bb2092a714399cabb32
Bryan Matias <bmm.matty@gmail.com>
2023-04-14 15:05:51 +0000

    btnlogin refactor

--- components/BtnLogin.tsx
+++ components/BtnLogin.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
 import { signIn } from 'next-auth/react'
 
-const BtnLogin = ({ provider, bgColor, txtColor }) => {
+export interface IBtnLogn {
+  provider: {name: string, id: string},
+   bgColor: string,
+   txtColor: string
+
+}
+
+const BtnLogin = ({ provider, bgColor, txtColor } : IBtnLogn) => {
   return (
     <div>
       <button
         className="btn w-100 my-2 py-3"
         style={{ background: `${bgColor}`, color: `${txtColor}` }}
-        onClick={() => signIn(provider.id)}
+        onClick={() => {
+          signIn(provider.id).catch((error) => console.log(error))
+        }}
       >
         Sign in with {provider.name}
       </button>
